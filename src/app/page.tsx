dd412bc65aeb9ead2a2b0bef668169bb5186542e
Sampath G.S.N <144051948+KethakiKarunathilake@users.noreply.github.com>
2024-04-20 00:01:49 +0530

    g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ export default async function Home() {
       <section className="home section" id="home">
         <div className="container">
           <div className="intro">
-            {/* <Image
+            <Image
               src="https://scontent.fcmb11-1.fna.fbcdn.net/v/t39.30808-6/433880495_1631931444301031_8837518557356348561_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGwp8I-XXGsDA3tv4CoYsu2Ar1v7Yobo9gCvW_tihuj2Ko4zaVJMEyvYm1grI2f2rb0jzr3zJlroWwBOfNOww2e&_nc_ohc=e7VXnzdzIyQAb64oDJL&_nc_zt=23&_nc_ht=scontent.fcmb11-1.fna&cb_e2o_trans=q&oh=00_AfCLbcBUMCw9_0Xka2aFtWOPyGy-q65icS8oIeLrOTCFpA&oe=66279C52"
               alt="Kethu image"
               className="shadow-dark"
               width={960}
               height={960}
-            /> */}
+            /> 
             <h1>Kethaki Karunathilake</h1>
             <p>Undergraduate- BSc.(Hons.) in Information Technology</p>
             <div className="social-links">
